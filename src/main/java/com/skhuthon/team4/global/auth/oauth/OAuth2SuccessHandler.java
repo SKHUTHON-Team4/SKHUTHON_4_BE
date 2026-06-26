00f21c4d5677949fc6d9a3c1e66a4bdfaaa0cfd3
Kim, Hangyeol <gyeol3474@gmail.com>
2026-06-26 21:49:07 +0900

    fix: redirect-uri HTTPS로 변경

--- src/main/java/com/skhuthon/team4/global/auth/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/skhuthon/team4/global/auth/oauth/OAuth2SuccessHandler.java
@@ -32,8 +32,8 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         log.info("OAuth2 로그인 성공 - memberId: {}", memberId);
 
         // 프론트엔드로 토큰과 함께 리다이렉트
-        String redirectUrl = "http://localhost:3000/oauth/callback?token=" + token;
-//        String redirectUrl = "https://gksruf.store/oauth/callback?token=" + token;
+//        String redirectUrl = "http://localhost:3000/oauth/callback?token=" + token;
+        String redirectUrl = "https://gksruf.store/oauth/callback?token=" + token;
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
 }
