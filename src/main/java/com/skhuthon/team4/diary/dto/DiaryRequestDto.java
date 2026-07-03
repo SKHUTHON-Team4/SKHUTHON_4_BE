17e59b8b6ebb5373aff700ffebd01cf068b94309
Kim, Hangyeol <gyeol3474@gmail.com>
2026-07-03 15:17:00 +0900

    revert: 일기 공백 제한 제거

--- src/main/java/com/skhuthon/team4/diary/dto/DiaryRequestDto.java
+++ src/main/java/com/skhuthon/team4/diary/dto/DiaryRequestDto.java
@@ -1,7 +1,6 @@
 package com.skhuthon.team4.diary.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record DiaryRequestDto(
@@ -12,7 +11,6 @@ public record DiaryRequestDto(
 
         @NotBlank(message = "내용을 입력해주세요.")
         @Size(min = 100, max = 500, message = "내용은 100자 이상 500자 이하로 입력해주세요.")
-        @Pattern(regexp = "^(?!\\s{10,}).*$", message = "공백을 10자 이상 연속으로 입력할 수 없어요.")
         String content,
 
         Boolean isPublic,
